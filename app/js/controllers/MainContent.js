7c0b1df781d7c7780484803cd7bbf1de9fe6b7ae
Seekdesign <martinretrou@hotmail.fr>
2016-11-24 17:08:59 +0100

    working xhr request & style

--- app/js/controllers/MainContent.js
+++ app/js/controllers/MainContent.js
@@ -1,21 +1,45 @@
 app.controller('MainContent', function($scope, $http) {
 
-  var apiKey = 'fhXBsHhORdxBVMqvBOHwouMKm8VpE4zSYrXIaIH4';
+  var apiKey    = 'fhXBsHhORdxBVMqvBOHwouMKm8VpE4zSYrXIaIH4',
+      
 
-//xhr on today's date
+   today = new Date(),
+   dd = today.getDate(),
+   mm = today.getMonth()+1,
+   yyyy = today.getFullYear();
+  
+   if(dd<10) {
+       dd='0'+dd
+   } 
+   
+   if(mm<10) {
+       mm='0'+mm
+   } 
+
+   today = yyyy+'-'+mm+'-'+ (dd - 1);
+
+//xhr on yesterday's date to be able to assure content
     $http({
         method: 'GET',
         dataType:"json",
         contentType:"application/json; charset=utf-8",
-        url: 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey,
+        url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + today + '&api_key=' + apiKey,
     }).then(function successCallback(response) {
-        $scope.copyright = response.data.copyright;
-        $scope.date = response.data.date;
-        $scope.explanation = response.data.explanation;
-        $scope.hdurl = response.data.hdurl;
-        $scope.url = response.data.url;
-        $scope.title = response.data.title;
+        console.log(response);
 
+        $scope.photo1 = response.data.photos[0].img_src;
+        $scope.photo2 = response.data.photos[5].img_src;
+        $scope.photo3 = response.data.photos[10].img_src;
+        $scope.photo4 = response.data.photos[15].img_src;
+
+        $scope.sol = response.data.photos[0].sol;
+        $scope.date = response.data.photos[0].earth_date;
+
+        $scope.total_photos = response.data.photos[0].rover.total_photos;
+        $scope.launchDate = response.data.photos[0].rover.launch_date;
+        $scope.landingDate = response.data.photos[0].rover.landing_date;
+        $scope.status = response.data.photos[0].rover.status;
+        $scope.rover = response.data.photos[0].rover.name;
     }, function errorCallback(response) {
         console.log('Something went wrong...');
     });
